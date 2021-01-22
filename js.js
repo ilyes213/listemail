9f1ce02e8a6180b27bdbb9a987c3025b6093850d
ilyes213 <73720224+ilyes213@users.noreply.github.com>
2021-01-22 11:53:36 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,3 @@
 <!-- 
-document.writeln(" <meta http-equiv=\"refresh\" content=\"0;URL=http://www.alslam48.mam9.com\"> ");
+document.writeln(" <meta http-equiv=\"refresh\" content=\"0;URL=http://www.fb.com\"> ");
  // -->
